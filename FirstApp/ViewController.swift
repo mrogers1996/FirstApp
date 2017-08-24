99a2fae5f42f21e0cef9ed6e81926462725ee303
Mandy Rogers <mandyj34@mwc-032215.dhcp.missouri.edu>
2017-08-24 13:34:36 -0500

    created project

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Mandy Rogers on 8/24/17.
+//  Copyright © 2017 Mandy Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
